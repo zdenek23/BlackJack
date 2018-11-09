03c369394948679ebb0d2090faa3db78020d7d9a
Zdenek Zeman <zzeman@student.tugraz.at>
2018-11-09 01:28:47 +0100

    upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// main.cpp
+// Author: Zdenek Zeman
+// Last change: 15-7-18
+
+#include "Game.h"
+//#include "Deck.h"
+
+int main()
+{
+    Game game;
+    game.deposit();
+    game.bettingRound();
+    game.play();
+    //game.printInfo();
+
+    return 0;
+}
